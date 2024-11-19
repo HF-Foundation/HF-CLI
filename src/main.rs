1b39bdd00d78405fd6f8c630aac8f255264e23ca
Luuk van Oijen <lazyluuk.channel@gmail.com>
2024-11-19 14:40:58 +0100

    add basic TargetTriplet parsing support

--- src/main.rs
+++ src/main.rs
@@ -2,17 +2,19 @@ use clap::{Parser, Subcommand};
 use std::path::PathBuf;
 use thiserror::Error;
 
+use hf_codegen::target::{Arch, CallingConvention, Os, Target};
+
 #[derive(Debug, Error)]
 enum ParseError {
     #[error("invalid target triplet")]
     InvalidTargetTriplet,
+    #[error("unknown host in target triplet")]
+    UnknownTargetTripletHost,
 }
 
 #[derive(Debug, Clone)]
 struct TargetTriplet {
-    host: String,
-    vendor: String,
-    system: String,
+    target: Target,
 }
 
 impl std::str::FromStr for TargetTriplet {
@@ -25,11 +27,51 @@ impl std::str::FromStr for TargetTriplet {
             return Err(ParseError::InvalidTargetTriplet);
         }
 
-        Ok(TargetTriplet {
-            host: parts[0].to_string(),
-            vendor: parts[1].to_string(),
-            system: parts[2].to_string(),
-        })
+        let host = parts[0].to_string();
+        let vendor = parts[1].to_string();
+        let system = parts[2].to_string();
+
+        let arch = match host.as_str() {
+            "x86" => Arch::X86,
+            "x86_64" => Arch::X86_64,
+            "wasm32" => Arch::Wasm32,
+            "wasm64" => Arch::Wasm64,
+            "aarch64" => Arch::Aarch64,
+            "riscv" => Arch::RiscV,
+            "mips" => Arch::Mips,
+            "powerpc" => Arch::PowerPc,
+            "sparc" => Arch::Sparc,
+            "z390" => Arch::Z390,
+            "m68k" => Arch::M68k,
+            "spirv" => Arch::SpirV,
+            "riscv32" => Arch::RiscV32,
+            "riscv64" => Arch::RiscV64,
+            "riscv128" => Arch::RiscV128,
+            _ => return Err(ParseError::UnknownTargetTripletHost),
+        };
+
+        let os = match system.as_str() {
+            "windows" => Some(Os::Windows),
+            "linux" => Some(Os::Linux),
+            "bsd" => Some(Os::Bsd),
+            "solaris" => Some(Os::Solaris),
+            "illumos" => Some(Os::Illumos),
+            "haiku" => Some(Os::Haiku),
+            "redox" => Some(Os::Redox),
+            "theseus" => Some(Os::Theseus),
+            _ => None,
+        };
+
+        let calling_convention = if let Some(os) = os {
+            os.calling_convention()
+        } else {
+            // TODO: Match system to calling convention
+            todo!()
+        };
+
+        let target = Target::new(arch, calling_convention);
+
+        Ok(TargetTriplet { target })
     }
 }
 
@@ -51,6 +93,7 @@ enum Command {
         #[arg(long, value_parser = clap::value_parser!(TargetTriplet))]
         target: Option<TargetTriplet>,
 
+        /// A list of files to compile.
         files: Vec<PathBuf>,
     },
 }
